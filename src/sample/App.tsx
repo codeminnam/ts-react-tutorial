4ce0ba534131c256aa97aed10d7e4e9dbec718c8
codeminnam <code.minnam@gmail.com>
2020-07-15 11:52:25 +0900

    Tidy up the structure & Add Redux

---
+++ src/sample/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Greetings from './Greetings';
+import Counter from './Counter';
+import MyForm from './MyForm';
+import ReducerSample from './ReducerSample';
+import { SampleProvider } from './SampleContext';
+
+function App() {
+  const onClick = (name: string) => {
+    console.log(name);
+  }
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
+  }
+  return (
+    <SampleProvider>
+      <ReducerSample />
+    </SampleProvider>
+  );
+}
+
+export default App;
